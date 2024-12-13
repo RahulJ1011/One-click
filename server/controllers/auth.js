804bda2ab337c3f947a1b0f8eb12ed5b28b14da8
rahulj1011 <rahuljr2022@gmail.com>
2024-12-13 21:53:21 +0530

    Added login routes

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,7 @@
 const user = require('../model/auth');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+const {OAuth2Client} = require('google-auth-library')
 const {generateAcessToken,generateRefreshToken} = require('../utils/TokenUtils')
 const register = async(req,res)=>
 {
@@ -72,7 +73,79 @@ const login = async(req,res)=>
 }
 
 
+const verifyGoogleToken = async(tokenId,clientId)=>
+{
+   const client = new OAuth2Client(clientId);
+   const ticket = await client.verifyIdToken({
+    idToken:tokenId,
+    audience:clientId
+   })
+   const payload = ticket.getPayload();
+   return payload;
+}
+
+const googleLogin = async(req,res)=>
+{
+    try
+    {
+        const {tokenId} = req.body;
+        const payload = await verifyGoogleToken(tokenId, GOOGLE_CLIENT_ID);
+        let user = await user.findOne({Email:payload.email});
+        if(!user)
+        {
+            user = new user({
+                Email:payload.email,
+            })
+            await user.save();
+        }
+        const tokenPayload = {
+            email:user.Email,
+            user_type:user.user_type
+        }
+        const accessToken = generateAccessToken(tokenPayload);
+        const refreshToken = generateRefreshToken(tokenPayload);
+    
+
+        res.cookie('accessToken', accessToken, {
+          httpOnly: true,
+          maxAge: 3600000, 
+          secure: true,   
+          sameSite: 'None' 
+        });
+        res.cookie('refreshToken', refreshToken, {
+          httpOnly: true,
+          maxAge: 259200000, 
+          secure: true,   
+          sameSite: 'None' 
+        });
+  
+        res.status(200).json({ message: 'Login successful', user });
+        
+    }
+    catch(err)
+    {
+        console.log(err);
+        return res.status(500).json({msg:"Internal Server Error"})
+    }
+}
+
+const logout = async(req,res)=>
+{
+    res.cookie('accessToken',null,{
+        httpOnly:true,
+        maxAge:-1,
+        scure:true,
+        sameSite:'None'
+    })
+    res.cookie('RefreshToken',null,{
+        httpOnly:true,
+        maxAge:-1,
+        scure:true,
+        sameSite:'None'
+    })
+
+    res.json({message:"Logout sucessfull"})
+}
 
 
-
-module.exports = {login,register}
+module.exports = {login,register,googleLogin,logout}
